9870203e52d3148d59e545657e64b11ddd19fc43
muhammadyasa <muhammadyasa307@gmail.com>
2024-03-02 09:26:36 +0500

    update

---
+++ app.js
@@ -0,0 +1,37 @@
+// chap 4
+
+let a, b, c;
+
+
+let illegal;  
+let my-variable;  
+let my variable;  
+let myVar;  
+let let;  
+
+
+let myVar;
+let _name;
+let $value;
+let camelCase;
+let camel_case;
+
+// chap 5
+
+let num1 = 5;
+let num2 = 3;
+let sum = num1 + num2;
+document.write("The sum is: " + sum);
+
+let num1 = 10;
+let num2 = 4;
+let subtract = num1 - num2;
+let multiply = num1 * num2;
+let divide = num1 / num2;
+let modulus = num1 % num2;
+
+document.write("<h2>Multiplication Table of 4</h2>");
+for (let i = 1; i <= 10; i++) {
+  document.write("4 × " + i + " = " + (4 * i) + "<br>");
+}
+
